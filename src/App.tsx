b0e0b9cdacd99c369286ad9ac8e8aaee91624b49
hugosundberg <hugosundberg98@gmail.com>
2024-12-05 16:26:18 +0100

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,16 @@ export default function App() {
           Hello, Tailwind with React!
         </h1>
       </div>
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-red-700">
+          Hello, Tailwind with React!
+        </h1>
+      </div>
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-red-700">
+          Hello, Tailwind with React!
+        </h1>
+      </div>
     </>
   );
 }
