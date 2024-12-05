b4b1a8a65d2b647f98488767c58234b5081b0431
hugosundberg <hugosundberg98@gmail.com>
2024-12-05 16:48:50 +0100

    nav

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -45,12 +45,42 @@ const Navigation = () => {
             />
           </svg>
         </button>
-        <input
-          type="text"
-          name="search"
-          id="search"
-          className="w-1/5 h-10 rounded-3xl"
-        />
+
+        {/* Search Bar*/}
+        <div className="relative w-80">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg
+              className="h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </div>
+          <input
+            type="search"
+            id="default-search"
+            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 dark:bg-gray-900 dark:border-opacity-0 dark:placeholder-gray-400 dark:text-white focus:outline-none"
+            placeholder="Search Teams, Players..."
+            required
+          />
+          <button
+            type="submit"
+            className="text-white absolute end-2.5 bottom-2.5 hover:bg-green-800 font-medium rounded-full text-sm px-4 py-2 dark:bg-gray-800 dark:hover:bg-green-900 focus:outline-none"
+          >
+            Search
+          </button>
+        </div>
+
+        {/* NAV MENU */}
         <div
           className="hidden w-full md:block md:w-auto"
           id="navbar-multi-level"
