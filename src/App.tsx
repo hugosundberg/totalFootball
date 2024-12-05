e73a4a129abaf7006ce71bbb5efbb2be37e2133b
hugosundberg <hugosundberg98@gmail.com>
2024-12-05 12:56:16 +0100

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import Navigation from "./Navigation";
+
 export default function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-700">
-        Hello, Tailwind with React!
-      </h1>
-    </div>
+    <>
+      <Navigation />
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-red-700">
+          Hello, Tailwind with React!
+        </h1>
+      </div>
+    </>
   );
 }
---
+++ src/Navigation.tsx
@@ -0,0 +1,9 @@
+const Navigation = () => {
+  return (
+    <div className="bg-green-800 h-20 flex text-white text text-4xl">
+      <div className="">TotalFootball</div>
+    </div>
+  );
+};
+
+export default Navigation;
