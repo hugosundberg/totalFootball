ad5a6585468c4cb5f1646077f672f40cbad1df92
hugosundberg <hugosundberg98@gmail.com>
2024-12-06 12:44:07 +0100

    env

--- src/pages/League.tsx
+++ src/pages/League.tsx
@@ -4,15 +4,32 @@ import logo from "../../assets/premierLeague.webp";
 const League = () => {
   return (
     <>
-      <div className="bg-blue-200 h-screen">
-        <div className="relative pt-20 bg-black h-full w-full">
-          <div className="bg-zinc-900 h-48 p-10 flex">
-            <div className="flex text-white">
-              <img src={logo} alt="" className="h-14" />
-              <div className="ml-4">
-                <h2 className="text-white text-2xl">Premier League</h2>
-                <p>England</p>
+      <div className="flex bg-black h-screen justify-center ">
+        <div className="relative pt-16 bg-black h-full w-full md:lg:max-w-screen-xl">
+          <div className="bg-zinc-800 mt-4 h-48 p-10 flex flex-row rounded-lg">
+            <div className="text-white flex flex-col">
+              <div className="flex">
+                <img src={logo} alt="" className="h-14" />
+                <div className="ml-4">
+                  <h2 className="text-white text-2xl">Premier League</h2>
+                  <p>England</p>
+                </div>
               </div>
+              <div className="text-white top-0 flex gap-10 pt-10">
+                <button>Table</button>
+                <button>Teams</button>
+                <button>Matches</button>
+                <button>Stats</button>
+              </div>
+            </div>
+          </div>
+          <div className="bg-zinc-800 mt-4 h-48 p-10 flex flex-row rounded-lg text-white">
+            <p>Matches</p>
+            <div className="text-white top-0 flex gap-10 pt-10">
+              <button>Table</button>
+              <button>Teams</button>
+              <button>Matches</button>
+              <button>Stats</button>
             </div>
           </div>
         </div>
