cf8ad55495aaef72023844055cdd47703d50a1d3
hugosundberg <hugosundberg98@gmail.com>
2024-12-06 10:08:51 +0100

    dev

--- src/pages/League.tsx
+++ src/pages/League.tsx
@@ -6,11 +6,14 @@ const League = () => {
     <>
       <div className="bg-blue-200 h-screen">
         <div className="relative pt-20 bg-black h-full w-full">
-          <div className="bg-zinc-900 h-48 p-10">
-            <div className="bg-white h-fit w-fit">
-              <img src={logo} alt="" className="h-10" />
+          <div className="bg-zinc-900 h-48 p-10 flex">
+            <div className="flex text-white">
+              <img src={logo} alt="" className="h-14" />
+              <div className="ml-4">
+                <h2 className="text-white text-2xl">Premier League</h2>
+                <p>England</p>
+              </div>
             </div>
-            <h2 className="text-white">Premier League</h2>
           </div>
         </div>
       </div>
