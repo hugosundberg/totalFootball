10b1a046731be682a7d2491bd84119dc6f549ba8
hugosundberg <hugosundberg98@gmail.com>
2024-12-13 00:26:35 +0100

    matches displayed

--- src/components/FixtureList.tsx
+++ src/components/FixtureList.tsx
@@ -1,11 +1,32 @@
 const FixtureList = ({ fixtures }: FixtureListProps) => {
-  if (!fixtures) return;
+  if (!fixtures) return null;
 
   return (
-    <div className="bg-slate-500 w-full h-10">
+    <div className="flex flex-col bg-slate-800 w-full h-fit gap-2">
       {fixtures.map((fixture, index) => (
-        <div key={index}>
-          {fixture.fixtureInfo?.referee || "Referee not available"}
+        <div key={index} className="h-20 bg-slate-400">
+          <div>{fixture.fixtureInfo?.date}</div>
+          <div className="grid grid-cols-custom-fixture gap-2 items-center">
+            <span className="justify-self-end text-center ">
+              {fixture.teams.home.name}
+            </span>
+            <img
+              src={fixture.teams.home.logo}
+              className="h-5"
+              alt="home-team-logo"
+            />
+            <div className="text-center">
+              {fixture.goals.home} - {fixture.goals.away}
+            </div>
+            <img
+              src={fixture.teams.away.logo}
+              className="h-5"
+              alt="away-team-logo"
+            />
+            <span className=" justify-self-starttext-center">
+              {fixture.teams.away.name}
+            </span>
+          </div>
         </div>
       ))}
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        "custom-fixture": "minmax(50px, 1fr) 25px 50px 25px minmax(50px, 1fr)",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
