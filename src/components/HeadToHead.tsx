db358bc44fa04687b2ec440393c471617d45202b
hugosundberg <hugosundberg98@gmail.com>
2025-01-23 17:47:57 +0100

    head to head styling

--- src/components/HeadToHead.tsx
+++ src/components/HeadToHead.tsx
@@ -1,14 +1,51 @@
 const HeadToHead = ({ fixture, headToHead }: HeadToHeadProps) => {
+  if (headToHead === undefined) {
+    return null;
+  }
   return (
-    <div className="justify-self-center w-11/12 bg-slate-800 p-4 rounded-2xl mt-5">
-      <h1 className="justify-self-center">Head To Head</h1>
-      <div>
-        <div className="flex justify-between">
-          <p>{fixture.teams.home.name}</p>
-          <p>{fixture.teams.away.name}</p>
+    <div className="flex flex-col bg-zinc-900 w-full h-fit rounded-2xl overflow-auto mb-10">
+      {headToHead.map((match, index) => (
+        <div
+          key={index}
+          className="h-fit hover:bg-zinc-800 hover:cursor-pointer"
+        >
+          <div className="flex justify-between p-6">
+            <p className="text-sm text-gray-400">
+              {new Date(match.fixtureInfo.date).toLocaleDateString()}
+            </p>
+            <div className="flex gap-2 items-center text-sm text-gray-400">
+              {match.league.name}
+              <img
+                src={match.league.logo}
+                className="h-8 bg-gray-400 p-1 rounded-full"
+                alt="league-logo"
+              />
+            </div>
+          </div>
+          <div className="grid grid-cols-custom-fixture gap-2 items-center pb-8">
+            <span className="justify-self-end text-center ">
+              {match.teams.home.name}
+            </span>
+            <img
+              src={match.teams.home.logo}
+              className="justify-self-center h-5"
+              alt="home-team-logo"
+            />
+            <div className="text-center">
+              {match.goals.home} - {match.goals.away}
+            </div>
+            <img
+              src={match.teams.away.logo}
+              className="justify-self-center h-5"
+              alt="away-team-logo"
+            />
+            <span className="justify-self-start text-center">
+              {match.teams.away.name}
+            </span>
+          </div>
+          <span className="block w-full h-0.5 bg-zinc-800" />
         </div>
-      </div>
-      <p>{}</p>
+      ))}
     </div>
   );
 };
