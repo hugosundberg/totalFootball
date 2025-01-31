dac0c1c68f4720a0643af3d868bf0a9df92f1d0b
hugosundberg <hugosundberg98@gmail.com>
2025-01-31 11:03:37 +0100

    team form complete

--- src/components/TeamOverview.tsx
+++ src/components/TeamOverview.tsx
@@ -5,14 +5,11 @@ const TeamOverview = ({ team, fixtures }: TeamOverviewProps) => {
 
   const [nextFixture, setNextFixture] = useState<Fixture | null>(null);
   const [currentForm, setCurrentForm] = useState<Fixture[]>();
-  const [index, setIndex] = useState<Number>(0);
 
   useEffect(() => {
     setNextFixture(getNextFixture(fixtures));
-    getCurrentForm(fixtures);
+    setCurrentForm(getCurrentForm(fixtures));
   }, [fixtures]);
-
-  console.log(fixtures);
 
   const getNextFixture = (fixtures: Fixture[]) => {
     const today = new Date();
@@ -25,26 +22,133 @@ const TeamOverview = ({ team, fixtures }: TeamOverviewProps) => {
     return null;
   };
 
-  const getCurrentForm = (fixtures: Fixture[]) => {};
+  const getCurrentForm = (fixtures: Fixture[]) => {
+    const today = new Date();
+    for (let index = 0; index < fixtures.length; index++) {
+      const fixtureDate = new Date(fixtures[index].fixtureInfo.date);
+      if (fixtureDate > today) {
+        if (index < 5) {
+          return fixtures.slice(0, index);
+        } else {
+          return fixtures.slice(index - 5, index);
+        }
+      }
+    }
+  };
 
-  console.log("Next fixture: ", getNextFixture(fixtures));
+  const dateFormatter = (date: string) => {
+    try {
+      const extractedDate = date.slice(0, 10);
+
+      const formattedDate = new Intl.DateTimeFormat("en-US", {
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+      }).format(new Date(extractedDate));
+
+      return formattedDate;
+    } catch (error) {
+      console.error("Invalid date format:", error);
+      return null;
+    }
+  };
+
+  const timeFormatter = (date: string) => {
+    try {
+      const extractedTime = date.slice(11, 16);
+
+      const [hour, minute] = extractedTime.split(":");
+      const formattedTime = new Intl.DateTimeFormat("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+      }).format(new Date().setHours(Number(hour), Number(minute)));
+
+      return formattedTime;
+    } catch (error) {
+      console.error("Invalid date format:", error);
+      return null;
+    }
+  };
 
   return (
     <div className="w-full">
-      <div className="bg-zinc-800 w-full h-full p-8 rounded-xl">
-        <p>Next match</p>
+      {/* Next match */}
+      <div className="bg-zinc-800 w-full p-6 rounded-xl">
+        <div className="flex justify-between">
+          <p>Next match</p>
+          <div className="flex items-center gap-2">
+            <p>{nextFixture?.league.name}</p>
+            <img
+              src={nextFixture?.league.logo}
+              alt=""
+              className="h-8 bg-white rounded-full p-1"
+            />
+          </div>
+        </div>
         <div className="flex justify-between mt-6 px-10">
           <div className="flex flex-col items-center gap-2">
             <img src={nextFixture?.teams.home.logo} alt="" className="h-10" />
             <p>{nextFixture?.teams.home.name}</p>
           </div>
-          <div>
-            <p>{nextFixture?.fixtureInfo.date}</p>
+          <div className="flex flex-col items-center gap-2">
+            {nextFixture && (
+              <>
+                <p className="text-2xl">
+                  {timeFormatter(nextFixture?.fixtureInfo.date)}
+                </p>
+                <p className="text-sm">
+                  {dateFormatter(nextFixture?.fixtureInfo.date)}
+                </p>
+              </>
+            )}
           </div>
           <div className="flex flex-col items-center gap-2">
             <img src={nextFixture?.teams.away.logo} alt="" className="h-10" />
             <p>{nextFixture?.teams.away.name}</p>
           </div>
+        </div>
+      </div>
+
+      {/* Current form */}
+      <div className="bg-zinc-800 w-full p-8 rounded-xl mt-4">
+        <p>Team form</p>
+        <div className="flex justify-between mt-6">
+          {currentForm?.map((fixture, index) => (
+            <div key={index} className="flex flex-col items-center gap-2">
+              {fixture.teams.away.teamID === team.id ? (
+                <div className="flex flex-col items-center gap-2">
+                  <p
+                    className={`px-2 rounded-lg ${
+                      fixture.teams.away.winner
+                        ? "bg-green-500"
+                        : fixture.teams.home.winner
+                          ? "bg-red-500"
+                          : "bg-gray-500"
+                    }`}
+                  >
+                    {fixture.goals.home} - {fixture.goals.away}
+                  </p>
+                  <img src={fixture.teams.home.logo} alt="" className="h-10" />
+                </div>
+              ) : (
+                <div className="flex flex-col items-center gap-2">
+                  <p
+                    className={`px-2 rounded-lg ${
+                      fixture.teams.home.winner
+                        ? "bg-green-500"
+                        : fixture.teams.away.winner
+                          ? "bg-red-500"
+                          : "bg-gray-500"
+                    }`}
+                  >
+                    {fixture.goals.home} - {fixture.goals.away}
+                  </p>
+                  <img src={fixture.teams.away.logo} alt="" className="h-10" />
+                </div>
+              )}
+            </div>
+          ))}
         </div>
       </div>
     </div>
