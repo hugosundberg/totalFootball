3c0203c9f5a40efa5ff03736c27969cfed7c269e
Hugo Sundberg <hugosundberg@Hugos-MacBook-Pro.local>
2025-03-11 10:56:16 +0100

    player stats table

--- src/components/PlayerMatchStats.tsx
+++ src/components/PlayerMatchStats.tsx
@@ -1,9 +1,16 @@
 const PlayerMatchStats = ({ stats }: PlayerMatchStatsProps) => {
 
-    console.log("PlayerMatchStats: ", stats);
-    
+  if (!stats) {
+    return <div>Loading...</div>;
+  }
 
-  return <div></div>;
+  console.log("PlayerMatchStats: ", stats);
+
+  return (
+    <div className="bg-red-500 w-full h-20">
+      <div> hello </div>
+    </div>
+  );
 };
 
 export default PlayerMatchStats;
