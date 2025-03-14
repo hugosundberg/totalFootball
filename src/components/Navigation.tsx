0102e50a329f83f6eb64d0c0212dcdc3fb96a80e
Hugo Sundberg <hugosundberg@Hugos-MacBook-Pro.local>
2025-03-14 10:24:06 +0100

    about page added

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -22,30 +22,6 @@ const Navigation = () => {
             Total Football
           </span>
         </a>
-        <button
-          data-collapse-toggle="navbar-multi-level"
-          type="button"
-          className="inline-flex items-center w-10 h-8 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-multi-level"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
 
         {/* NAV MENU */}
         <div
@@ -55,7 +31,7 @@ const Navigation = () => {
           <ul className="flex flex-col font-medium md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-transparent dark:border-gray-700">
             <li>
               <a
-                href="#"
+                href="/totalFootball"
                 className="block py-2 px-3 dark:text-white rounded md:bg-transparent md:p-0  hover:underline"
                 aria-current="page"
               >
@@ -64,7 +40,7 @@ const Navigation = () => {
             </li>
             <li>
               <a
-                href="totalFootball/about"
+                href="/totalFootball/about"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 About
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,9 @@
 const About = () => {
   return (
-    <div>
-      <div className="bg-black text-2xl font-bold text-center mt-4">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque sequi
-        explicabo temporibus inventore quam, nostrum dolore ullam amet eligendi
-        voluptatem alias, quas nesciunt magni laboriosam ducimus odit ea
-        repudiandae ipsa.
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <div className="bg-black w-full h-screen text-white text-center pt-20 px-40">
+        <p className="text-4xl font-bold my-5">Hi! My name is Hugo. </p>
+        <p className="text-2xl">I am a webdeveloper with a passion for design. This is currently under development using React, TypeScript and Tailwind CSS. I am using API-FOOTBALL to fetch data. Please visit <a className="text-blue-400 hover:underline" href="https://hugosundberg.github.io/hugo/">my portfolio website</a> to checkout more of my work.</p>
       </div>
     </div>
   );
